d09e85f2d85a910e6e17ef889b7fdbc85cbda5e4
EdlbauerM22047 <EdlbauerM22047@sus.htl-grieskirchen.at>
2024-09-20 15:41:46 +0200

    Noch ein = hinzufegügt, Fertig mit Programm

--- src/naturalNumberCalculator.java
+++ src/naturalNumberCalculator.java
@@ -6,7 +6,7 @@ public class naturalNumberCalculator {
 
     EratosthenesPrimeSieve primeSieve=new EratosthenesPrimeSieve(obergrenze);
         for(int i=4;i<=obergrenze;i+=2){
-            for(int n=2;n<i/2;n++){
+            for(int n=2;n<=i/2;n++){
                     if(primeSieve.isPrime(n)){
                         if(primeSieve.isPrime(i-n)){
                             System.out.println(i+ " summe: "+ i +" = "+ n +" + "+ (i-n));break;
