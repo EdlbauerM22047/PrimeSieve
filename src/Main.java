47311f052f78564e66d1647b389ddda3c99b695f
EdlbauerM22047 <EdlbauerM22047@sus.htl-grieskirchen.at>
2024-09-20 15:40:25 +0200

    bei Schleife noch = hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         EratosthenesPrimeSieve primeSieve=new EratosthenesPrimeSieve(100);
         primeSieve.printPrimes();
+
+        naturalNumberCalculator numberCalculator=new naturalNumberCalculator(10);
+        numberCalculator.AlgorithmusfürSummeAusPrimzahlen();
     }
 }
--- src/naturalNumberCalculator.java
+++ src/naturalNumberCalculator.java
@@ -5,11 +5,11 @@ public class naturalNumberCalculator {
     public void AlgorithmusfürSummeAusPrimzahlen(){
 
     EratosthenesPrimeSieve primeSieve=new EratosthenesPrimeSieve(obergrenze);
-        for(int i=4;i<obergrenze;i+=2){
+        for(int i=4;i<=obergrenze;i+=2){
             for(int n=2;n<i/2;n++){
                     if(primeSieve.isPrime(n)){
                         if(primeSieve.isPrime(i-n)){
-                            System.out.println(i+ " summe: "+ i +" = "+ n +" + "+ (i-n));
+                            System.out.println(i+ " summe: "+ i +" = "+ n +" + "+ (i-n));break;
                         }
                     }
             }
